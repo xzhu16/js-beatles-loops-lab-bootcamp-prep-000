c4810ad17e022cc4fee693a23b7f7f38c04b0265
Xi Zhu <zxjucy2012@gmail.com>
2019-04-29 13:30:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var result = [];
   var count = 0;
-  while (count < facts.length)
+  while (count < facts.length) {
+    
+  }
 }
